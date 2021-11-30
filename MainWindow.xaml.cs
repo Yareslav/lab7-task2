8f9dfd980fe516282b62e414188f9aacfa0282f9
Yareslav <80545801+Yareslav@users.noreply.github.com>
2021-11-30 14:24:38 +0200

    try2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,60 +14,108 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 namespace Hme2
 {
-    class MyMatrix
-    {
-        public int[,] value;
-        public void Fill()
-        {
-            var random = new Random();
-            for (var i = 0; i < value.GetLength(0); i++)
-            {
-                for (var j = 0; j < value.GetLength(1); j++)
-                {
-                    value[i, j] = random.Next(-100, 100);
-                }
-            }
-        }
-        public void Create(int columns, int rows)
-        {
-            value = new int[columns, rows];
-        }
-        public string GetSize()
-        {
-            string result = "";
-
-            for (var i = 0; i < value.GetLength(0); i++)
-            {
-                for (var j = 0; j < value.GetLength(1); j++)
-                {
-                    result += value[i, j] + " ";
-                }
-            }
-            return result;
-        }
-    }
     public partial class MainWindow : Window
     {
-        private MyMatrix matrix = new MyMatrix();
+        private Store store = new Store();
         public MainWindow()
         {
             InitializeComponent();
         }
-        private void Fill_Click(object sender, RoutedEventArgs e)
+        private void ResetPoroductData(object sender, RoutedEventArgs e)
         {
-            int columns = 1;
-            int rows = 1;
-            if (int.TryParse(Rows.Text, out rows) && int.TryParse(Columns.Text, out columns))
+            string Name = ProductName.Text;
+            string Store = ProductStore.Text;
+            int Price;
+            if (int.TryParse(ProductPrice.Text, out Price))
             {
-                matrix.Create(columns, rows);
-                matrix.Fill();
+                if (Name=="")
+                {
+                    MessageBox.Show("Name is empty");
+                    return;
+                }
+                if (Store=="")
+                {
+                    MessageBox.Show("Store is empty");
+                    return;
+                }
+                MessageBox.Show("Product was succesfully updated");
+                store.Change(new Article(Name, Store, Price));
             }
-            else MessageBox.Show("невозможно создать матрицу");
+            else MessageBox.Show("price is in wrong format");
         }
-        private void Print_Click(object sender, RoutedEventArgs e)
+        private void SearchByIndex(object sender, RoutedEventArgs e)
         {
-            if (matrix.value != null) Field.Text = matrix.GetSize();
-            else MessageBox.Show("матриця пуста");
+            int index;
+            if (int.TryParse(IndexField.Text,out index))
+            {
+                var product= store.GetProductByIndex(index);
+                if (product!=null)
+                {
+                    FillProductData(product);
+                } else MessageBox.Show($"There is no product with id {index}");
+            } else MessageBox.Show("Index is in wrong format");
+        }
+        private void SearchByName(object sender, RoutedEventArgs e)
+        {
+            var name = NameField.Text;
+            var product = store.GetProductByName(name);
+            if (name != "") {
+                if (product!=null)
+                {
+                    FillProductData(product);
+                } else MessageBox.Show($"There is no product with name {name}");
+            }
+            else MessageBox.Show("Enter the name of product");
+        }
+        private void FillProductData(Article product)
+        {
+            ProductPrice.Text = product.Price + "";
+            ProductStore.Text = product.Store;
+            ProductName.Text = product.Name;
+        }
+    }
+    class Article
+    {
+        public string Name;
+        public string Store;
+        public int Price;
+        public Article(string Name,string Store,int Price)
+        {
+            this.Name = Name;
+            this.Store = Store;
+            this.Price = Price;
+        }
+    }
+    class Store
+    {
+        public int SelectedStoreId;
+        public Article[] value=new Article[] {new Article("Banana","Monkey Shop",30),new Article("apple","Vanila meyvey",20),new Article("cheese","Rokfor",80)};
+        public Article GetProductByName(string Name)
+        {
+            Article result=null;
+            for (var i = 0; i <value.Length; i++)
+            {
+                if (Name.ToLower() == value[i].Name.ToLower())
+                {
+                    result = value[i];
+                    this.SelectedStoreId = i;
+                }
+            }
+            return result;
+        }
+        public Article GetProductByIndex(int ind)
+        {
+            bool contains = ind > value.Count() - 1;
+            if (contains) return null;
+            else
+            {
+                this.SelectedStoreId = ind;
+                return value[ind];
+            }
+        }
+        public void Change(Article product)
+        {
+            value[SelectedStoreId] = product;
         }
     }
 }
